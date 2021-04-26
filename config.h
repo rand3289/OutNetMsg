659d3b1f62b19388e2fab1ad5d302391fa7b2c30
rand3289 <rand3289@rand3289.com>
2021-04-26 02:37:52 -0400

    playing with html

--- config.h
+++ config.h
@@ -5,12 +5,12 @@
 // without knowing the "friend word" friend requests will be denied
 struct Config {
     std::string friendWord; // friendword
-    int refreshTime;        // OutNet update interval seconds
-    int readWriteTimeout;   // server send() recv() timeout in seconds
+    int refreshTime = 600;        // OutNet update interval seconds
+    int readWriteTimeout = 3;   // server send() recv() timeout in seconds
     uint32_t outIP;         // OutNet IP
     uint16_t outPort;       // OutNet port number
     uint16_t serverPort=2778;    // server port number
-    uint16_t trayPort;      // tray icon notification port number
+//    uint16_t trayPort;      // tray icon notification port number // get it from OutNet - tray should register
 
     bool load();
     bool save();
